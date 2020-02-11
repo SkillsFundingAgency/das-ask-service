89458c529603c4be97875e556a479bba2af0fdaa
Alan Burns <adburns57@gmail.com>
2020-02-11 12:31:17 +0000

    include DeliveryPartners in build

--- src/SFA.DAS.ASK.Database/AddDeliveryPartners.sql
+++ src/SFA.DAS.ASK.Database/AddDeliveryPartners.sql
@@ -12,7 +12,7 @@ INSERT INTO DeliveryPartners (Id, Name, Status, UkPrn) VALUES
 (1,'B L Training Limited','Live', '10000486'),
 (2,'Workpays Limited','Live', '10037289'),
 (3,'Education Development Trust','Live', '10001298'),
-(4,'CXK Limited','Live', '10001648'),
+(4,'CXK Limited','Live', '10001648')
 
 SET IDENTITY_INSERT DeliveryPartners OFF
 
