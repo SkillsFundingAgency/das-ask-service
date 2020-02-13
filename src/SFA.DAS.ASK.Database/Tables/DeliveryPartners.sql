fa89c579622e562d488bb9d1253c5f8396696186
Dave Gouge <davegouge@gmail.com>
2020-02-13 08:57:23 +0000

    Fixed up DeliveryPartners ID field

--- src/SFA.DAS.ASK.Database/Tables/DeliveryPartners.sql
+++ src/SFA.DAS.ASK.Database/Tables/DeliveryPartners.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [DeliveryPartners](
-	[Id] [uniqueidentifier] IDENTITY (1,1) PRIMARY KEY,
+	[Id] [uniqueidentifier] PRIMARY KEY,
 	[Name] [nvarchar](256) NOT NULL, 
     [Status] [int] NOT NULL,
     [UkPrn] [int] NOT NULL
