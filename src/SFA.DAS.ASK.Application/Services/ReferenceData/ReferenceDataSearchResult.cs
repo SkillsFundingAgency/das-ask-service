79590ee73e780e37a42c9d3f17298dbcf9165afa
Dave Gouge <davegouge@gmail.com>
2020-02-19 08:38:33 +0000

    Added Id field to Ref Data search result class

--- src/SFA.DAS.ASK.Application/Services/ReferenceData/ReferenceDataSearchResult.cs
+++ src/SFA.DAS.ASK.Application/Services/ReferenceData/ReferenceDataSearchResult.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace SFA.DAS.ASK.Application.Services.ReferenceData
 {
     public class ReferenceDataSearchResult
     {
+        public Guid? Id { get; set; }
         public string Name { get; set; }
         public int Type { get; set; }
         public int SubType { get; set; }
