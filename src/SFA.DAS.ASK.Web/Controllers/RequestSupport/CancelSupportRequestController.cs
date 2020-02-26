66f4b15926168e3cfa216ca3e7ad84048eb4110f
Dave Gouge <davegouge@gmail.com>
2020-02-26 13:53:02 +0000

    Added logging to identify TEST cancel issue

--- src/SFA.DAS.ASK.Web/Controllers/RequestSupport/CancelSupportRequestController.cs
+++ src/SFA.DAS.ASK.Web/Controllers/RequestSupport/CancelSupportRequestController.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using SFA.DAS.ASK.Application.Handlers.RequestSupport.CancelSupportRequest;
 using SFA.DAS.ASK.Web.Infrastructure.ModelStateTransfer;
 using SFA.DAS.ASK.Web.ViewModels.RequestSupport;
@@ -13,10 +15,12 @@ namespace SFA.DAS.ASK.Web.Controllers.RequestSupport
     public class CancelSupportRequestController : Controller
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<CancelSupportRequestController> _logger;
 
-        public CancelSupportRequestController(IMediator mediator)
+        public CancelSupportRequestController(IMediator mediator, ILogger<CancelSupportRequestController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpGet("cancel-request/{requestId}")]
@@ -32,11 +36,13 @@ namespace SFA.DAS.ASK.Web.Controllers.RequestSupport
         [ExportModelState]
         public async Task<IActionResult> Cancel(Guid requestId, CancelSupportRequestViewModel vm)
         {
+            _logger.LogInformation($"CancelSupportRequestController.Cancel: requestId: {requestId}, vm: {JsonConvert.SerializeObject(vm)}");
             if (!ModelState.IsValid)
             {
                 return RedirectToAction("Index", new {requestId, vm.ReturnAction, vm.ReturnController});
             }
-
+            _logger.LogInformation($"CancelSupportRequestController.Cancel: ModelState is Valid");
+            
             if (!vm.ConfirmedCancel.Value)
             {
                 return RedirectToAction(vm.ReturnAction, vm.ReturnController, new {requestId = requestId});
