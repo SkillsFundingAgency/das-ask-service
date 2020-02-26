de17949bcef16f43eacd53f0c1024753c45db89c
Ben <ben1.stone@education.gov.uk>
2020-02-26 08:40:22 +0000

    in progress

---
+++ src/SFA.DAS.ASK.Web.UnitTests/Controllers/WhenRenderingTheYourDetailsController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using NSubstitute;
+using NUnit.Framework;
+using SFA.DAS.ASK.Application.Handlers.RequestSupport.GetSupportRequest;
+using SFA.DAS.ASK.Web.Controllers.RequestSupport;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.ASK.Web.UnitTests.Controllers
+{
+    
+    public class WhenRenderingTheYourDetailsController
+    {
+        protected IMediator Mediator;
+
+        private YourDetailsController sut;
+        [SetUp]
+        public void Arrange()
+        {
+            
+            Mediator = Substitute.For<IMediator>();
+            
+            Mediator.Send(new GetTempSupportRequest(Arg.Any<Guid>())).Returns();
+
+            sut = new YourDetailsController(Mediator);
+        }
+
+        [Test]
+        Then
+
+    }
+}
