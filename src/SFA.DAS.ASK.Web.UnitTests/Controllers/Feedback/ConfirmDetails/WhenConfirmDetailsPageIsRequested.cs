81f809732f482fdb34e8bf1ae452ccb0c62be066
Ben Stone <ben1.stone@education.gov.uk>
2020-03-04 13:17:58 +0000

    confirm details page

---
+++ src/SFA.DAS.ASK.Web.UnitTests/Controllers/Feedback/ConfirmDetails/WhenConfirmDetailsPageIsRequested.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.ASK.Web.Controllers.Feedback;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SFA.DAS.ASK.Web.UnitTests.Controllers.Feedback.ConfirmDetails
+{
+    [TestFixture]
+    public class WhenConfirmDetailsPageIsRequested
+    {
+        private FeedbackConfirmDetailsController _controller;
+
+        [SetUp]
+        public void Arrange()
+        {
+            //_controller = new FeedbackConfirmDetailsController();
+        }
+
+        [Test]
+        public void ThenAViewResultSReturned()
+        {
+           // var result = _controller.Index();
+          //  result.As<ViewResult>().ViewName.Should().Be("~/Views/Feedback/ConfirmDetails.cshtml");
+        }
+    }
+}
---
+++ src/SFA.DAS.ASK.Web/ViewModels/Feedback/ConfirmDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using SFA.DAS.ASK.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ASK.Web.ViewModels.Feedback
+{
+    public class ConfirmDetailsViewModel
+    {
+        public string EstablishmentName { get; set; }
+        public string YourFullName { get; set; }
+        public string DateOfActivity { get; set; }
+        public List<VisitActivity> Activities { get; set; }
+        public Guid FeedbackId { get; set; }
+
+        public ConfirmDetailsViewModel(VisitFeedback fullVisit)
+        {
+            FeedbackId = fullVisit.Id;
+            var visit = fullVisit.Visit;
+
+            EstablishmentName = visit.SupportRequest.Organisation.OrganisationName;
+            YourFullName = $"{visit.SupportRequest.OrganisationContact.FirstName} {visit.SupportRequest.OrganisationContact.LastName}";
+            DateOfActivity = visit.VisitDate.ToString("dd/MM/yyyy");
+            Activities = visit.Activities;
+            
+        }
+    }
+}
