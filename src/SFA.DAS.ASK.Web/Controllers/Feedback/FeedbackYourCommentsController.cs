8912aca4b302421c7fbb4ab5501e1f52f243e636
Ben Stone <ben1.stone@education.gov.uk>
2020-03-05 09:02:26 +0000

    in progress

---
+++ src/SFA.DAS.ASK.Web/Controllers/Feedback/FeedbackYourCommentsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ASK.Application.Handlers.Feedback.GetVisitFeedback;
+using SFA.DAS.ASK.Web.ViewModels.Feedback;
+
+namespace SFA.DAS.ASK.Web.Controllers.Feedback
+{
+    public class FeedbackYourCommentsController : Controller
+    {
+        private IMediator _mediator;
+
+        public FeedbackYourCommentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("/feedback/your-comments/{feedbackId}")]
+        public async Task<IActionResult> Index(Guid feedbackId)
+        {
+            var feedback = await _mediator.Send(new GetVisitFeedbackRequest(feedbackId, true));
+
+            var viewModel = new YourCommentsViewModel(feedback);
+
+            return View("~/Views/Feedback/YourComments.cshtml", viewModel);
+        }
+
+        [HttpPost("feedback/your-comments/{feedbackId}")]
+        public async Task<IActionResult> SubmitFeedback(YourCommentsViewModel yourComments)
+        {
+            return RedirectToAction("Index", "FeedbackComplete", new { feedbackId = yourComments.FeedbackId });
+        }
+    }
+}
---
+++ src/SFA.DAS.ASK.Web/ViewModels/Feedback/YourCommentsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using SFA.DAS.ASK.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ASK.Web.ViewModels.Feedback
+{
+    public class YourCommentsViewModel
+    {
+        public Guid FeedbackId { get; set; }
+
+        public string CommentsBestThings { get; set; }
+        public string CommentsCouldBeImproved { get; set; }
+        public string CommentsAdditional { get; set; }
+        public YourCommentsViewModel()
+        {
+
+        }
+        public YourCommentsViewModel(VisitFeedback feedback)
+        {
+            FeedbackId = feedback.Id;
+
+            CommentsBestThings = "";
+            CommentsCouldBeImproved = "";
+            CommentsAdditional = "Additional comments";
+        }
+    }
+}
