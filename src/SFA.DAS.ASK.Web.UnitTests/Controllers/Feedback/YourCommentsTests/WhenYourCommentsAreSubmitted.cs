2ea5cad89304455993be825f1b1a12224c583921
Ben Stone <ben1.stone@education.gov.uk>
2020-03-06 13:00:19 +0000

    updates unit tests

--- src/SFA.DAS.ASK.Web.UnitTests/Controllers/Feedback/YourCommentsTests/WhenYourCommentsAreSubmitted.cs
+++ src/SFA.DAS.ASK.Web.UnitTests/Controllers/Feedback/YourCommentsTests/WhenYourCommentsAreSubmitted.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using NUnit.Framework;
 using SFA.DAS.ASK.Application.Handlers.Feedback.GetVisitFeedback;
+using SFA.DAS.ASK.Application.Handlers.Feedback.SaveVisitFeedback;
 using SFA.DAS.ASK.Data.Entities;
 using SFA.DAS.ASK.Web.Controllers.Feedback;
 using SFA.DAS.ASK.Web.Controllers.Feedback.ViewModels;
@@ -14,31 +15,37 @@ using System.Threading.Tasks;
 
 namespace SFA.DAS.ASK.Web.UnitTests.Controllers.Feedback.YourCommentsTests
 {
-    //[TestFixture]
-    //public class WhenYourCommentsAreSubmitted : FeedbackTestBase
-    //{
-    //    private IMediator _mediator;
-    //    private Guid feedbackId;
-    //    private FeedbackYourCommentsController _controller;
+    [TestFixture]
+    public class WhenYourCommentsAreSubmitted : FeedbackTestBase
+    {
+        
+        private FeedbackYourCommentsController _controller;
 
-    //    [SetUp]
-    //    public void Arrange()
-    //    {
-    //        _mediator = Substitute.For<IMediator>();
-    //        feedbackId = Guid.Parse("BC2BFFD8-6B20-4BEC-BF33-F83C970DD73E");
-    //        _controller = new FeedbackYourCommentsController(_mediator);
-    //    }
+        [SetUp]
+        public void Arrange()
+        {
+            _controller = new FeedbackYourCommentsController(Mediator);
+        }
 
-    //    [Test]
-    //    public async Task ThenUserIsRedirectedToTheFeedbackCompletePage()
-    //    {
-    //        _mediator.Send(Arg.Any<GetVisitFeedbackRequest>()).Returns(GetVisitFeedback());
+        [Test]
+        public async Task ThenUserIsRedirectedToTheFeedbackCompletePage()
+        {
+            Mediator.Send(Arg.Any<GetVisitFeedbackRequest>()).Returns(GetVisitFeedback());
 
-    //        var result = await _controller.Index(feedbackId, new YourCommentsViewModel());
+            var result = await _controller.Index(FEEDBACK_ID, new YourCommentsViewModel());
+            
+            result.As<RedirectToActionResult>().ControllerName.Should().Be("FeedbackComplete");
+        }
 
-    //        result.As<ViewResult>().ViewName.Should().Be("~/Views/Feedback/Complete.cshtml");
-    //    }
+        [Test]
+        public async Task ThenTheSaveVisitFeedbackCommandIsCalledOnce()
+        {
+            Mediator.Send(Arg.Any<GetVisitFeedbackRequest>()).Returns(GetVisitFeedback());
 
-    
-    //}
+            var result = await _controller.Index(FEEDBACK_ID, new YourCommentsViewModel());
+
+            Mediator.Received(1).Send(Arg.Any<SaveVisitFeedbackRequest>());
+        }
+
+    }
 }
